b66f9d2a7125d177fc27dcccbad4f00d6fb1a80a
Ariq Fraser <ariqfraser@gmail.com>
2024-02-07 23:26:07 +0000

    updating stuff

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,11 @@
+return {
+    { "rebelot/kanagawa.nvim" },
+    { "tanvirtin/monokai.nvim" },
+    { "folke/tokyonight.nvim" },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "monokai_pro",
+        },
+    },
+}
